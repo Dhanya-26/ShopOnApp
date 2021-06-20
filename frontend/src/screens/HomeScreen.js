d133c71a295887ce3d1f893b174ec41f22c36489
Dhanya <adhanya2611@gmail.com>
2021-06-20 13:26:25 +0530

    first commit

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,59 @@
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col } from 'react-bootstrap'
+
+import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import Paginate from '../components/Paginate'
+import Meta from '../components/Meta'
+import ProductCarousel from '../components/ProductCarousel'
+
+import { listProducts } from '../actions/productActions'
+
+
+const HomeScreen = ({ match }) => {
+   const keyword = match.params.keyword
+
+   const pageNumber = match.params.pageNumber || 1
+
+    const dispatch = useDispatch()
+
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products, pages, page} = productList
+
+    useEffect(() => {
+      dispatch(listProducts(keyword, pageNumber))
+    }, [dispatch, keyword, pageNumber])
+
+    return (
+        <>
+       <Meta/>
+        {!keyword ? <ProductCarousel/> : <Link t-='/' className='btn btn-dark'>
+ 
+            Go Back
+          
+        </Link>}
+          <h1>Popular Products</h1>
+          { loading ? <Loader/> 
+          : error 
+          ? <Message variant='danger'>{error}</Message>
+          : 
+          <>
+          <Row>
+             {products.map(product => (
+                 <Col key={product._id} sm={12} md={6} lg={4} xl={4} style={{marginBottom: '30px'}}>
+                     <Product product={product}/>
+                 </Col>
+             ))}
+          </Row>
+          <Paginate pages={pages} page={page} keyword={keyword ? keyword : ''}/>
+          </>
+          }
+            
+        </>
+    )
+}
+
+export default HomeScreen
---
+++ frontend/src/screens/RegisterScreen.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import FormContainer from '../components/FormContainer'
+import { register } from '../actions/userActions'
+
+const RegisterScreen = ({ location, history }) => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [message, setMessage] = useState(null)
+
+    const dispatch = useDispatch()
+
+    const userRegister = useSelector(state => state.userRegister)
+    const { loading, error, userInfo } = userRegister
+
+    const redirect = location.search 
+    ? location.search.split('=')[1] 
+    : '/'
+
+    useEffect(() => {
+        if(userInfo) {
+            history.push(redirect)
+        }
+    }, [history, userInfo, redirect])
+    
+    const submitHandler = (e) => {
+        e.preventDefault()
+        if(password !== confirmPassword){
+            setMessage('Passwords do not match')
+        } else {
+            //DISPATCH REGISTER
+        dispatch(register(name, email, password))
+        }
+        
+    }
+
+    return (
+        <FormContainer>
+            <h1>Sign Up</h1>
+            {message && <Message variant='danger'>{message}</Message>}
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader/>}
+            <Form onSubmit={submitHandler}>
+                <Form.Group controlId='name' style={{ marginBottom: "14px"}}>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control autoComplete="off"
+                       type='name' 
+                       placeholder='Enter Name' 
+                       value={name}
+                       onChange={(e) => setName(e.target.value)}>
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='email' style={{ marginBottom: "14px"}}>
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control autoComplete="off"
+                       type='email' 
+                       placeholder='Enter email' 
+                       value={email}
+                       onChange={(e) => setEmail(e.target.value)}>
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='password' style={{ marginBottom: "14px"}}>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control autoComplete="off"
+                       type='password' 
+                       placeholder='Enter password' 
+                       value={password}
+                       onChange={(e) => setPassword(e.target.value)}>
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='confirmPassword' style={{ marginBottom: "14px"}}>
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control autoComplete="off"
+                       type='password' 
+                       placeholder='Confirm password' 
+                       value={confirmPassword}
+                       onChange={(e) => setConfirmPassword(e.target.value)}>
+                    </Form.Control>
+                </Form.Group>
+
+                <Button type='submit' variant='primary' style={{ margin: '10px' }}>
+                    Register
+                </Button>
+            </Form>
+
+            <Row className='py-3'>
+                <Col>
+                    Have an Account?{' '} 
+                    <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+                         Login
+                    </Link>
+                </Col>
+            </Row>
+        </FormContainer>
+    )
+}
+
+export default RegisterScreen
